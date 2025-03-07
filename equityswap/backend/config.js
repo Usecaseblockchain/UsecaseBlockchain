aaac00f0cbc65baba53bec554a34ab6695771f83
Usecaseblockchai <usecaseblockchain@gmail.com>
2025-03-07 04:56:09 +0000

    Working backend

--- equityswap/backend/config.js
+++ equityswap/backend/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     ARBITRUM_RPC_URL: "https://arb1.arbitrum.io/rpc",
-    PRIVATE_KEY: "process.env.PRIVATE_KEY",
+    PRIVATE_KEY: "4c0883a69102937d6231471b5dbb6204fe5129617082798bd2e19c3f66540b43",
     CONTRACT_ADDRESS: "0xYourDeployedContractAddress",
 };
--- equityswap/backend/hardhat.config.js
+++ equityswap/backend/hardhat.config.js
@@ -2,10 +2,11 @@ require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
 
 module.exports = {
-    solidity: "0.8.19",
+    solidity: "0.8.20",
     networks: {
         arbitrum: {
             url: "https://arb1.arbitrum.io/rpc",
             accounts: [process.env.PRIVATE_KEY],
         },
     },
+}
